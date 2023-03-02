be0beee28c3771b96a0544aa60560de6a743589e
Brune <abrune.osteo@gmail.com>
2023-03-02 15:20:08 +0100

    useContext updated

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./assets/Header"
+import Button from "./assets/Button"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Button />
+        </div>
+    )
+}
+
+export default App
